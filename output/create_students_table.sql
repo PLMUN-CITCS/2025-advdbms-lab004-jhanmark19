635d354f6ea4bfcbf55c9d0312342d8d80c146a8
jhanmark19 <loretejhanmark_bsit@plmun.edu.ph>
2025-02-18 14:55:57 +0800

    lab004

--- output/create_students_table.sql
+++ output/create_students_table.sql
@@ -2,6 +2,7 @@ Use `UniversityDB`;
 
 CREATE TABLE `Students` (
    `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
+   
    `FirstName` VARCHAR(50) NOT NULL,
    `LastName` VARCHAR(50) NOT NULL,
    `EnrollmentDate` DATE
