fe6a25904ea18252408c2170bac1c1cb9b70dcfe
jhanmark19 <loretejhanmark_bsit@plmun.edu.ph>
2025-02-18 15:07:50 +0800

    lab004

--- output/alter_students_table.sql
+++ output/alter_students_table.sql
@@ -1,4 +1,4 @@
 Use `UniversityDB`;
 
 ALTER TABLE `Students`
-ADD `Email` VARCHAR(100);
+ADD COLUMN `Email` VARCHAR(100) UNIQUE;
--- output/create_students_table.sql
+++ output/create_students_table.sql
@@ -1,8 +1,8 @@
-Use `UniversityDB`;
+USE `UniversityDB`;
 
 CREATE TABLE `Students` (
    `StudentID` INT PRIMARY KEY AUTO_INCREMENT,
    `FirstName` VARCHAR(50) NOT NULL,
    `LastName` VARCHAR(50) NOT NULL,
-   `EnrollmentDate` DATE,
+   `EnrollmentDate` DATE
 );
